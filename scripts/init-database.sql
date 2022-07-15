c3c159a2dfd682d51e95459c4c453646a6ef0c14
guieg <guiegle@hotmail.com>
2022-07-15 17:53:07 -0300

    docker-compose

--- scripts/init-database.sql
+++ scripts/init-database.sql
@@ -11,13 +11,24 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema sgc
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `sgc` DEFAULT CHARACTER SET utf8 ;
+CREATE SCHEMA IF NOT EXISTS `sgc` DEFAULT CHARACTER SET utf8mb3 ;
 USE `sgc` ;
 
 -- -----------------------------------------------------
--- Table `sgc`.`Usuario`
+-- Table `sgc`.`modelo_fabricante`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sgc`.`Usuario` (
+CREATE TABLE IF NOT EXISTS `sgc`.`modelo_fabricante` (
+  `modelo` VARCHAR(45) NOT NULL,
+  `fabricante` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`modelo`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `sgc`.`usuario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `sgc`.`usuario` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(45) NOT NULL,
   `senha` VARCHAR(45) NOT NULL,
@@ -27,96 +38,87 @@ CREATE TABLE IF NOT EXISTS `sgc`.`Usuario` (
   `telefone` VARCHAR(13) NOT NULL,
   `endereco` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 105
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `sgc`.`Vendedor`
+-- Table `sgc`.`cliente`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sgc`.`Vendedor` (
+CREATE TABLE IF NOT EXISTS `sgc`.`cliente` (
+  `u_id` INT NOT NULL,
+  PRIMARY KEY (`u_id`),
+  CONSTRAINT `fk_Cliente_Usuário`
+    FOREIGN KEY (`u_id`)
+    REFERENCES `sgc`.`usuario` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `sgc`.`vendedor`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `sgc`.`vendedor` (
   `u_id` INT NOT NULL,
   `gerente` TINYINT NOT NULL,
   PRIMARY KEY (`u_id`),
   CONSTRAINT `fk_Vendedor_Usuário1`
     FOREIGN KEY (`u_id`)
-    REFERENCES `sgc`.`Usuario` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `sgc`.`usuario` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `sgc`.`Cliente`
+-- Table `sgc`.`venda`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sgc`.`Cliente` (
-  `u_id` INT NOT NULL,
-  PRIMARY KEY (`u_id`),
-  CONSTRAINT `fk_Cliente_Usuário`
-    FOREIGN KEY (`u_id`)
-    REFERENCES `sgc`.`Usuario` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `sgc`.`Venda`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sgc`.`Venda` (
+CREATE TABLE IF NOT EXISTS `sgc`.`venda` (
   `c_id` INT NOT NULL,
   `v_id` INT NOT NULL,
   `num_nota_fiscal` VARCHAR(45) NOT NULL,
   `data` DATE NOT NULL,
   `forma_pagamento` SMALLINT NOT NULL,
+  PRIMARY KEY (`num_nota_fiscal`),
   INDEX `fk_Venda_Cliente1_idx` (`c_id` ASC) VISIBLE,
   INDEX `fk_Venda_Vendedor1_idx` (`v_id` ASC) VISIBLE,
-  PRIMARY KEY (`num_nota_fiscal`),
   CONSTRAINT `fk_Venda_Cliente1`
     FOREIGN KEY (`c_id`)
-    REFERENCES `sgc`.`Cliente` (`u_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    REFERENCES `sgc`.`cliente` (`u_id`),
   CONSTRAINT `fk_Venda_Vendedor1`
     FOREIGN KEY (`v_id`)
-    REFERENCES `sgc`.`Vendedor` (`u_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `sgc`.`vendedor` (`u_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `sgc`.`modelo_fabricante`
+-- Table `sgc`.`automovel`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sgc`.`modelo_fabricante` (
-  `modelo` VARCHAR(45) NOT NULL,
-  `fabricante` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`modelo`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `sgc`.`Automovel`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sgc`.`Automovel` (
+CREATE TABLE IF NOT EXISTS `sgc`.`automovel` (
   `chassi` VARCHAR(17) NOT NULL,
   `cor` VARCHAR(45) NOT NULL,
   `ano` INT NOT NULL,
   `valor` DOUBLE NOT NULL,
-  `modelo_fabricante_modelo` VARCHAR(45) NOT NULL,
+  `mf_modelo` VARCHAR(45) NOT NULL,
+  `v_nf` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`chassi`),
-  INDEX `fk_Automovel_modelo_fabricante1_idx` (`modelo_fabricante_modelo` ASC) VISIBLE,
+  INDEX `fk_Automovel_modelo_fabricante1_idx` (`mf_modelo` ASC) VISIBLE,
+  INDEX `fk_Automovel_Venda1_idx` (`v_nf` ASC) VISIBLE,
   CONSTRAINT `fk_Automovel_modelo_fabricante1`
-    FOREIGN KEY (`modelo_fabricante_modelo`)
-    REFERENCES `sgc`.`modelo_fabricante` (`modelo`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    FOREIGN KEY (`mf_modelo`)
+    REFERENCES `sgc`.`modelo_fabricante` (`modelo`),
+  CONSTRAINT `fk_Automovel_Venda1`
+    FOREIGN KEY (`v_nf`)
+    REFERENCES `sgc`.`venda` (`num_nota_fiscal`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `sgc`.`Emplacamento`
+-- Table `sgc`.`emplacamento`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sgc`.`Emplacamento` (
+CREATE TABLE IF NOT EXISTS `sgc`.`emplacamento` (
   `placa` INT NOT NULL,
   `valor` DOUBLE NOT NULL,
   `despachante_nome` VARCHAR(45) NOT NULL,
@@ -126,49 +128,26 @@ CREATE TABLE IF NOT EXISTS `sgc`.`Emplacamento` (
   INDEX `fk_Emplacamento_Automóvel1_idx` (`a_chassi` ASC) VISIBLE,
   CONSTRAINT `fk_Emplacamento_Automóvel1`
     FOREIGN KEY (`a_chassi`)
-    REFERENCES `sgc`.`Automovel` (`chassi`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `sgc`.`automovel` (`chassi`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `sgc`.`Itens`
+-- Table `sgc`.`itens`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sgc`.`Itens` (
+CREATE TABLE IF NOT EXISTS `sgc`.`itens` (
   `id` INT NOT NULL,
   `nome` VARCHAR(45) NOT NULL,
   `descricao` VARCHAR(45) NOT NULL,
-  `a_chassi` VARCHAR(17) NULL,
-  INDEX `fk_Acessorios_Automóvel1_idx` (`a_chassi` ASC) VISIBLE,
+  `a_chassi` VARCHAR(17) NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
+  INDEX `fk_Acessorios_Automóvel1_idx` (`a_chassi` ASC) VISIBLE,
   CONSTRAINT `fk_Acessorios_Automóvel1`
     FOREIGN KEY (`a_chassi`)
-    REFERENCES `sgc`.`Automovel` (`chassi`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `sgc`.`possui`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sgc`.`possui` (
-  `v_nf` VARCHAR(45) NOT NULL,
-  `a_chassi` VARCHAR(17) NOT NULL,
-  PRIMARY KEY (`v_nf`, `a_chassi`),
-  INDEX `fk_possui_Automóvel1_idx` (`a_chassi` ASC) VISIBLE,
-  CONSTRAINT `fk_possui_Venda1`
-    FOREIGN KEY (`v_nf`)
-    REFERENCES `sgc`.`Venda` (`num_nota_fiscal`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_possui_Automóvel1`
-    FOREIGN KEY (`a_chassi`)
-    REFERENCES `sgc`.`Automovel` (`chassi`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `sgc`.`automovel` (`chassi`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
