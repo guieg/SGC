2679d6f8faa9a12fe87255b6ca8ff8fa99e744ee
Diego Filgueiras <diegofilbal@ufrn.edu.br>
2022-07-20 10:58:52 -0300

    Atualiza deleção de Vendedor no Controller

--- src/controller/vendedor.controller.js
+++ src/controller/vendedor.controller.js
@@ -1,5 +1,6 @@
 const vendedorDAO = require('../model/DAO/vendedor.dao');
 const Vendedor = require('../model/vendedor.model');
+const usuarioDAO = require('../model/DAO/usuario.dao');
 
 
 async function listarVendedores() {
@@ -10,17 +11,15 @@ async function getVendedor(id) {
     return await vendedorDAO.recuperaVendedorPorId(id);
 }
 
-
 async function deleteVendedor(id) {
+    await usuarioDAO.deletaUsuarioPorId(id);
     return await vendedorDAO.deletaVendedorPorId(id);
 }
-
 
 async function postVendedor(body) {
     let novoVendedor = new Vendedor({id: body.id, isGerente: body.isGerente});
     return await vendedorDAO.inserirVendedor(novoVendedor);
 }
-
 
 
 module.exports = {listarVendedores, getVendedor, deleteVendedor, postVendedor}
