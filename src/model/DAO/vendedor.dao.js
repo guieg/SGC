d4ed10fa30bd2bd92da7c03ada2d8fa03817cb06
Diego Filgueiras <diegofilbal@ufrn.edu.br>
2022-07-20 11:05:30 -0300

    Adiciona DAO de Vendedor

---
+++ src/model/DAO/vendedor.dao.js
@@ -0,0 +1,40 @@
+const db = require('../../utils/db')
+const Vendedor = require('../vendedor.model')
+const UsuarioDAO = require('./usuario.dao')
+
+
+async function inserirVendedor(vendedor) {
+    let query = "INSERT INTO vendedor SET ?";
+    connection = db.connect();
+    connection.query(query, vendedor, function(err) {
+        if(err) throw err;
+    });
+    connection.end();
+}
+
+async function listarVendedor() {
+    let query = "SELECT * FROM vendedor";
+    connection = db.connect();
+    let response = await connection.query(query);
+    connection.end();
+    let vendedores = [];
+    for (let index = 0; index < response.length; index++) {
+        vendedores.push(new Vendedor(response[index]));
+    }
+    return vendedores;
+}
+
+async function deletaVendedorPorId(id) {
+    let query = "DELETE FROM vendedor WHERE `u_id` = "+ db.mysql.escape(id);
+    connection = db.connect();
+    let response = await connection.query(query);
+    connection.end();
+    let vendedores = [];
+    for (let index = 0; index < response.length; index++) {
+        vendedores.push(new Vendedor(response[index]));
+    }
+    return vendedores;
+}
+
+
+module.exports = {inserirVendedor, listarVendedor, deletaVendedorPorId}
